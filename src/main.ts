2df5840e4fe4249e44acecf815a9f18aeb832fd0
Stewart Orr <stewart.orr@gmail.com>
2026-02-26 17:02:58 +0000

    chore: commit vite test files

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+// src/main.ts
+import { soCalendar } from "./js/soCalendar";
+import "./scss/soCalendar.scss"
+document.addEventListener('DOMContentLoaded', (event) => {
+  const calendar = new soCalendar();
+});
